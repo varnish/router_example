c4a617e873e182c0d3dce3ad9a1c061b68706063
Magnus Persson <nergal@nergal.se>
2022-10-18 16:10:17 +0200

    Updated vcli command.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 export VARNISH_CONTROLLER_CLI_PASSWORD=test
 vcli='docker run --mount source=router_example,target=/home/varnish/ --network router_example_default --rm -it quay.io/varnish-software/varnish-controller-cli:latest'
 vcli login http://api.example.com:8002 -u test
 vcli tag add prod
 vcli agent tag 1,2 -t 1 -y
-vcli vcl add web.vcl web.vcl
+vcli file add web.vcl web.vcl
 vcli domain add mysite.example.com
 vcli dep add prod -t 1 --max 2
 vcli rr add video --lookup-order=random --http --health-path=/ping --health-interval=1s --debug-headers=true --health-timeout 1s --health-window-size 3 --health-threshold 2
